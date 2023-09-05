22fa16d440f3057287df89016d9379768f6e884e
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-09-05 13:48:33 +0300

    feat: create header motion consts
    Created and exported animation-scripts for header

--- src/motionConsts/motionConsts.js
+++ src/motionConsts/motionConsts.js
@@ -1,3 +1,97 @@
 const genAnimDuration = 0.5;
 
-export { genAnimDuration };
+const headerTitleAnim = {
+	disabled: {
+		x: 0,
+		opacity: 1,
+		transition: {
+			duration: genAnimDuration,
+			type: "spring",
+			delay: genAnimDuration / 2,//to show title after Navigation been closed
+		}
+	},
+	enabled: {
+		x: '-150%',
+		opacity: 0,
+		transition: {
+			duration: genAnimDuration,
+			type: "spring",
+		},
+	}
+};
+
+const navigationAnim = {
+	disabled: {
+		y: '-100%',
+		transition: {
+			duration: genAnimDuration,
+			type: "spring",
+			staggerChildren: 0.05,
+			staggerDirection: -1,
+		}
+	},
+	enabled: {
+		y: 0,
+		transition: {
+			duration: genAnimDuration,
+			type: "spring",
+			staggerChildren: 0.07,
+			delayChildren: 0.2
+		}
+	}
+}
+
+const navigationItemAnim = {
+	disabled: {
+		y: 50,
+		opacity: 0,
+		transition: {
+			y: { stiffness: 1000 },
+			duration: 0.5,
+		}
+	},
+	enabled: {
+		y: 0,
+		opacity: 1,
+		transition: {
+			y: { stiffness: 1000, velocity: -100 },
+			duration: 0.5,
+		}
+	}
+};
+
+const burgerAnim = {
+	disabled: {
+		rotate: 0,
+		transition: {
+			duration: genAnimDuration,
+			type: "spring",
+		}
+	},
+	enabled: {
+		rotate: -90,
+		transition: {
+			duration: genAnimDuration,
+			type: "spring",
+		}
+	}
+}
+
+const burgerLineAnim = {
+	disabled: {
+		width: '100%',
+		transition: {
+			duration: genAnimDuration,
+			type: "spring",
+		}
+	},
+	enabled: widthValue => ({
+		width: widthValue,
+		transition: {
+			duration: genAnimDuration,
+			type: "spring",
+		}
+	})
+};
+
+export { genAnimDuration, headerTitleAnim, navigationAnim, navigationItemAnim, burgerAnim, burgerLineAnim };
