ede778c026739c58787fcaf9fe1cca5b187c8aa5
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-09-06 20:40:38 +0300

    feat(header): implement Home component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Header from './header/header.jsx';
 import { useStore } from '../store/store.js';
+import Home from './home/home.jsx';
 
 export default function App() {
 	return (
 		<div className='wrapper'>
 			<Header></Header>
+			<Home />
 		</div>
 	)
 }
