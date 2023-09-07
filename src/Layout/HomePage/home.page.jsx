5713a98e8adc09ddc1a4e976e7dfc55d127fed40
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-09-07 17:37:37 +0300

    feat: bind HomePage and HourSchedule to store
    Make components use store data and render data according to it

--- src/Layout/HomePage/home.page.jsx
+++ src/Layout/HomePage/home.page.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './home.page.less';
 import PageContainer from '../../components/page-container/page-container.jsx';
 import HourSchedule from '../../components/hour-schedule/hour-schedule.jsx';
+import { useVisitorsStore } from '../../store/store.js';
 
 export default function HomePage() {
 	const date = new Date();
 	const weekDaysArr = ['Понеділок', 'Вівторок', 'Середа', 'Четвер', "П'ятниця", 'Субота', 'Неділя'];
+
+	const visitorsList = useVisitorsStore(state => state.visitors);
+	const [todayVisitors, setTodayVisitors] = useState(
+		visitorsList.filter(li => li.schedule[0].isActive === true)
+	);
+	const [hoursArr, setHoursArr] = useState(
+		[...new Set(todayVisitors.map(el => el.schedule[0].specifiedTime))]
+	);
+
+	function getVisitorsListByHour(hour) {
+		return todayVisitors.filter(el => el.schedule[0].specifiedTime === hour);
+	}
 
 	return (
 		<main className='home'>
 			<PageContainer>
 				<div className="home__body">
 					<h2 className="page-title">
-						{`${weekDaysArr[date.getDay()]}, ${date.getDate()}`}
+						{`${weekDaysArr[date.getDay() - 1]}, ${date.getDate()}`}
 					</h2>
 
-					<HourSchedule hour={'12:00'} />
+					{
+						hoursArr.map((hour, id) => (
+							<HourSchedule key={id} hour={hour} visitorsList={getVisitorsListByHour(hour)} />
+						))
+					}
 
 				</div>
 			</PageContainer>
--- src/components/hour-schedule/hour-schedule.jsx
+++ src/components/hour-schedule/hour-schedule.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './hour-schedule.less';
 import VisitorCart from '../visitor-cart/visitor-cart.jsx';
 
-
 export default function HourSchedule({ hour, visitorsList }) {
 	return (
 		<div className="time-section">
@@ -12,12 +11,9 @@ export default function HourSchedule({ hour, visitorsList }) {
 			</div>
 
 			<ul className="time-section__visitors-list">
-				<VisitorCart name={'Сік Добрий'} subscriptionCounter={10} />
-				<VisitorCart name={'Сік Злий'} subscriptionCounter={1} />
-				<VisitorCart name={'Сік Злющий'} subscriptionCounter={2} />
-				<VisitorCart name={'Сік Садочок'} subscriptionCounter={0} />
-				<VisitorCart name={'Ранковий'} subscriptionCounter={3} />
-
+				{visitorsList.map((li, id) => (
+					<VisitorCart key={id} name={li.name} subscriptionCounter={li.subscription} />
+				))}
 			</ul>
 
 		</div>
