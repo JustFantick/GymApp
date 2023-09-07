28c55bb9ada1f9224a06022b12a4074c173c5376
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-09-07 19:18:51 +0300

    feat(home): modify VisitorCart component
    Counter & slider render depends on props. Also added opportunity to chose a theme, green by default

--- src/components/visitor-cart/visitor-cart.jsx
+++ src/components/visitor-cart/visitor-cart.jsx
@@ -3,40 +3,47 @@ import { motion } from 'framer-motion';
 import './visitor-cart.less';
 import QuickIncreaseButton from '../quick-increase-btn/quick-increase-btn.jsx';
 
-export default function VisitorCart({ name, subscriptionCounter }) {
-	const [isVisitorOpen, setIsVisitorOpen] = useState(false);
+export default function VisitorCart({ name, subscriptionCounter, theme = "green", minimized = false }) {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const cartCounter = !minimized &&
+		//Motion didn't used coz it doesn't allow animate border-radius
+		<div className="visitor-cart__subscription-counter" data-isopen={isOpen}>
+			{subscriptionCounter}
+		</div>;
+	const cartSlider = !minimized &&
+		<motion.div className="visitor-cart__slider increase-value-list"
+			variants={{
+				open: { x: 0 },
+				closed: { x: '100%' }
+			}}
+			animate={isOpen ? 'open' : 'closed'}
+			transition={{ ease: "backInOut", duration: 0.5 }}
+		>
+			{
+				[1, 2, 3, 4].map((li, id) => (
+					<QuickIncreaseButton key={id}
+						coefficient={li}
+						theme={subscriptionCounter === 0 ? 'yellow' : 'green'}
+						onClick={() => console.log(`Clicked +${li}`)}
+					/>
+				))
+			}
+		</motion.div>;
+
+	const expiringStatus = subscriptionCounter === 0 ? 'expired-subscription' : '';
 
 	return (
-		<motion.li
-			className={`visitor-cart ${subscriptionCounter === 0 ? 'expired-subscription' : ''}`}
-			onClick={() => setIsVisitorOpen(!isVisitorOpen)}
+		<motion.div
+			className={`visitor-cart ${expiringStatus} ${theme}`}
+			onClick={() => setIsOpen(!isOpen)}
 		>
 			<div className="visitor-cart__name">{name}</div>
 
-			{/* Motion didn't used coz it doesn't allow animate border-radius  */}
-			<div className="visitor-cart__subscription-counter" data-isopen={isVisitorOpen}>
-				{subscriptionCounter}
-			</div>
+			{cartCounter}
 
-			<motion.div className="visitor-cart__slider increase-value-list"
-				variants={{
-					open: { x: 0 },
-					closed: { x: '100%' }
-				}}
-				animate={isVisitorOpen ? 'open' : 'closed'}
-				transition={{ ease: "backInOut", duration: 0.5 }}
-			>
-				{
-					[1, 2, 3, 4].map((li, id) => (
-						<QuickIncreaseButton key={id}
-							coefficient={li}
-							theme={subscriptionCounter === 0 ? 'yellow' : 'green'}
-							onClick={() => console.log(`Clicked +${li}`)}
-						/>
-					))
-				}
-			</motion.div>
+			{cartSlider}
 
-		</motion.li>
+		</motion.div>
 	)
 }
