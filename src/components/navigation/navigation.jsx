0b4e91c76290ee28231020a0137d9b065b4ca2ec
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-09-08 12:31:10 +0300

    feat(header): set links to other pages
    Navigation links wrapped by NavLink component that transfers user to corresponding page

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -3,29 +3,34 @@ import { motion } from 'framer-motion';
 import './navigation.less';
 
 import { navigationItemAnim } from '../../motionConsts/motionConsts';
-const navItems = [
-	{ text: 'Головна ', isCurrent: true },
-	{ text: 'Відвідувачі ', isCurrent: false },
-	{ text: 'Реєстрація ', isCurrent: false },
-	{ text: 'Календар ', isCurrent: false }
-];
+import { homePath, visitorsPath, registrationPath, calendarPath } from '../../Layout/routing.jsx';
+import { NavLink } from 'react-router-dom';
 
 export const Navigation = forwardRef((props, ref) => {
+	const navItems = [
+		{ text: 'Головна ', isCurrent: true, linkUrl: homePath },
+		{ text: 'Відвідувачі ', isCurrent: false, linkUrl: visitorsPath },
+		{ text: 'Реєстрація ', isCurrent: false, linkUrl: registrationPath },
+		{ text: 'Календар ', isCurrent: false, linkUrl: calendarPath }
+	];
+
 	return (
 		<nav className="header-body__nav nav" ref={ref}>
 			{
 				navItems.map((item, id) => (
-					<motion.div className="nav__item" key={id}
-						data-iscurrent={item.isCurrent}
-						variants={navigationItemAnim}
-						whileHover={{ scale: 1.1 }}
-						whileTap={{ scale: 0.95 }}
-					>
-						{item.text}
-					</motion.div>
+					<NavLink to={item.linkUrl} key={id}>
+						<motion.div className="nav__item"
+							data-iscurrent={item.isCurrent}
+							variants={navigationItemAnim}
+							whileHover={{ scale: 1.1 }}
+							whileTap={{ scale: 0.95 }}
+						>
+							{item.text}
+						</motion.div>
+					</NavLink>
 				))
 			}
-		</nav>
+		</nav >
 	)
 });
 
