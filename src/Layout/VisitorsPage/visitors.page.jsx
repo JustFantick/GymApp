c92e038961089bbd99dd408b689f4bc3056ae18b
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-09-08 12:19:58 +0300

    feat(visitors): create VisitorsPage component

---
+++ src/Layout/VisitorsPage/visitors.page.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './visitors.page.less';
+import SearchField from '../../components/search-field/search-field.jsx';
+import VisitorCart from '../../components/visitor-cart/visitor-cart.jsx';
+import { useVisitorsStore } from '../../store/store';
+import PageContainer from '../../components/page-container/page-container.jsx';
+
+export default function VisitorsPage() {
+	const visitorsList = useVisitorsStore(state => state.visitors);
+
+	return (
+		<div className='visitors'>
+			<SearchField></SearchField>
+
+			<PageContainer>
+				<div className="visitors__list">
+					{visitorsList.map((li, id) => (
+						<VisitorCart key={id} name={li.name} minimized={true} />
+					))}
+				</div>
+			</PageContainer>
+
+		</div>
+	)
+}
