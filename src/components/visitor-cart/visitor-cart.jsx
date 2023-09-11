e7b8d254f8ceb6e5bd376e972ba0f3c9c517bb4b
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-09-11 10:51:53 +0300

    refactor: remove unnecessary code
    Removed "minimized" prop and added default value for "subscriptionCounter" prop. Simplified render layout

--- src/components/visitor-cart/visitor-cart.jsx
+++ src/components/visitor-cart/visitor-cart.jsx
@@ -3,35 +3,36 @@ import { motion } from 'framer-motion';
 import './visitor-cart.less';
 import QuickIncreaseButton from '../quick-increase-btn/quick-increase-btn.jsx';
 
-export default function VisitorCart({ name, subscriptionCounter, theme = "green", minimized = false }) {
+export default function VisitorCart({ name, subscriptionCounter = null, theme = "green" }) {
 	const [isOpen, setIsOpen] = useState(false);
-
-	const cartCounter = !minimized &&
-		//Motion didn't used coz it doesn't allow animate border-radius
-		<div className="visitor-cart__subscription-counter" data-isopen={isOpen}>
-			{subscriptionCounter}
-		</div>;
-	const cartSlider = !minimized &&
-		<motion.div className="visitor-cart__slider increase-value-list"
-			variants={{
-				open: { x: 0 },
-				closed: { x: '100%' }
-			}}
-			animate={isOpen ? 'open' : 'closed'}
-			transition={{ ease: "backInOut", duration: 0.5 }}
-		>
-			{
-				[1, 2, 3, 4].map((li, id) => (
-					<QuickIncreaseButton key={id}
-						coefficient={li}
-						theme={subscriptionCounter === 0 ? 'yellow' : 'green'}
-						onClick={() => console.log(`Clicked +${li}`)}
-					/>
-				))
-			}
-		</motion.div>;
-
 	const expiringStatus = subscriptionCounter === 0 ? 'expired-subscription' : '';
+
+	const subscriptionCounterBlock =
+		<>
+			{/* Motion didn't used coz it doesn't allow animate border-radius */}
+			<div className="visitor-cart__subscription-counter" data-isopen={isOpen}>
+				{subscriptionCounter}
+			</div>
+
+			<motion.div className="visitor-cart__slider increase-value-list"
+				variants={{
+					open: { x: 0 },
+					closed: { x: '100%' }
+				}}
+				animate={isOpen ? 'open' : 'closed'}
+				transition={{ ease: "backInOut", duration: 0.5 }}
+			>
+				{
+					[1, 2, 3, 4].map((li, id) => (
+						<QuickIncreaseButton key={id}
+							coefficient={li}
+							theme={subscriptionCounter === 0 ? 'yellow' : 'green'}
+							onClick={() => console.log(`Clicked +${li}`)}
+						/>
+					))
+				}
+			</motion.div>
+		</>;
 
 	return (
 		<motion.div
@@ -40,9 +41,11 @@ export default function VisitorCart({ name, subscriptionCounter, theme = "green"
 		>
 			<div className="visitor-cart__name">{name}</div>
 
-			{cartCounter}
-
-			{cartSlider}
+			{
+				//When "subscriptionCounter" unset by props
+				//dont show "subscriptionCounter" and "slider"
+				!(subscriptionCounter === null) && subscriptionCounterBlock
+			}
 
 		</motion.div>
 	)
