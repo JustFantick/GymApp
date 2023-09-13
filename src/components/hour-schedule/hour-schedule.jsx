464712b109f7ef9121af04055ffd045c7aeeeec3
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-09-13 20:24:28 +0300

    fix(home): change HourSchedule props list
    Set default null value for "addBtnOnClick", added new "showSubscriptionCounter". Implemented them into layout

--- src/components/hour-schedule/hour-schedule.jsx
+++ src/components/hour-schedule/hour-schedule.jsx
@@ -2,20 +2,37 @@ import React from 'react';
 import './hour-schedule.less';
 import VisitorCart from '../visitor-cart/visitor-cart.jsx';
 
-export default function HourSchedule({ hour, visitorsList, addBtnOnClick }) {
+export default function HourSchedule(
+	{ hour, visitorsList, addBtnOnClick = null, showSubscriptionCounter = true }
+) {
 	return (
 		<div className="time-section">
 			<div className="time-section__top">
 				<div className="time-section__time">{hour}</div>
-				<img className="time-section__add-btn"
-					onClick={addBtnOnClick}
-					src={require('../../img/add-btn.svg')} alt="svg1" />
+				{
+					addBtnOnClick &&
+					<img className="time-section__add-btn"
+						onClick={addBtnOnClick}
+						src={require('../../img/add-btn.svg')} alt="svg1"
+					/>
+				}
 			</div>
 
 			<ul className="time-section__visitors-list">
-				{visitorsList.map((li, id) => (
-					<VisitorCart key={id} name={li.name} subscriptionCounter={li.subscription} />
-				))}
+				{
+					showSubscriptionCounter ?
+						visitorsList.map((li, id) => (
+							<VisitorCart key={id} name={li.name} subscriptionCounter={li.subscription} />
+						))
+						:
+						visitorsList.map((li, id) => (
+							<VisitorCart key={id}
+								name={li.name}
+								subscriptionCounter={li.subscription}
+								showSubscriptionCounter={false}
+							/>
+						))
+				}
 			</ul>
 
 		</div>
