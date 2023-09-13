fc295b7db77ad204e18a0009af8f26e6fc08ec0b
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-09-13 20:25:09 +0300

    feat(calendar): add visitors list render

--- src/Layout/CalendarPage/calendar.page.jsx
+++ src/Layout/CalendarPage/calendar.page.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './calendar.page.less';
 import PageContainer from '../../components/page-container/page-container.jsx';
 import Calendar from '../../components/calendar/calendar.jsx';
+import { useVisitorsStore } from '../../store/store';
+import HourSchedule from '../../components/hour-schedule/hour-schedule.jsx';
 
 export default function CalendarPage() {
+	const visitorsList = useVisitorsStore(state => state.visitors);
+	const [todayVisitors, setTodayVisitors] = useState(
+		visitorsList.filter(li => li.schedule[0].isActive === true)
+	);
+	const [hoursArr, setHoursArr] = useState(
+		[...new Set(todayVisitors.map(el => el.schedule[0].specifiedTime))]
+	);
+
+	function getVisitorsListByHour(hour) {
+		return todayVisitors.filter(el => el.schedule[0].specifiedTime === hour);
+	}
+
 	return (
 		<div className="calendar-page">
 			<div className="calendar-page__calendar">
@@ -12,7 +26,15 @@ export default function CalendarPage() {
 
 			<PageContainer>
 				<div className="calendar-page__visitors-list">
-					Visitors List
+					{
+						hoursArr.map((hour, id) => (
+							<HourSchedule key={id}
+								hour={hour}
+								visitorsList={getVisitorsListByHour(hour)}
+								showSubscriptionCounter={false}
+							/>
+						))
+					}
 				</div>
 			</PageContainer>
 
