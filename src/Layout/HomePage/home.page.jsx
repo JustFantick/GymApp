771b5d6a0fa1495a9f62f7c76dab9d62caacfd05
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-09-16 18:54:52 +0300

    feat(home): implement PopupAddVisitor

--- src/Layout/HomePage/home.page.jsx
+++ src/Layout/HomePage/home.page.jsx
@@ -3,10 +3,8 @@ import './home.page.less';
 import PageContainer from '../../components/page-container/page-container.jsx';
 import HourSchedule from '../../components/hour-schedule/hour-schedule.jsx';
 import { useVisitorsStore } from '../../store/store.js';
-import PopupContainer from '../../components/popup-container/popup-container.jsx';
-import VisitorCart from '../../components/visitor-cart/visitor-cart.jsx';
 import { SectionTitle } from '../../components/section-title/section-title.jsx';
-import SearchField from '../../components/search-field/search-field.jsx';
+import PopupAddVisitor from '../../components/popup-add-visitor/popup-add-visitor.jsx';
 
 export default function HomePage() {
 	const date = new Date();
@@ -25,46 +23,40 @@ export default function HomePage() {
 	}
 
 	const [isPopupOpen, setIsPopupOpen] = useState(false);
-	const [searchValue, setSearchValue] = useState('');
+	function addVisitor(visitorName) {
+		console.log(visitorName);
+		setIsPopupOpen(false);
+	}
 
 	return (
-		<main className='home'>
-			<PageContainer>
-				<div className="home__body">
-					<SectionTitle>
-						{`${weekDaysArr[date.getDay() - 1]}, ${date.getDate()}`}
-					</SectionTitle>
+		<>
+			<main className='home'>
+				<PageContainer>
+					<div className="home__body">
+						<SectionTitle>
+							{`${weekDaysArr[date.getDay() - 1]}, ${date.getDate()}`}
+						</SectionTitle>
 
-					{
-						hoursArr.map((hour, id) => (
-							<HourSchedule key={id}
-								addBtnOnClick={() => setIsPopupOpen(true)}
-								hour={hour}
-								visitorsList={getVisitorsListByHour(hour)} />
-						))
-					}
+						{
+							hoursArr.map((hour, id) => (
+								<HourSchedule key={id}
+									addBtnOnClick={() => setIsPopupOpen(true)}
+									hour={hour}
+									visitorsList={getVisitorsListByHour(hour)} />
+							))
+						}
+					</div>
 
-				</div>
-			</PageContainer>
+				</PageContainer>
 
-			<PopupContainer isOpen={isPopupOpen} closePopup={() => setIsPopupOpen(false)}>
-				<div className="search">
-					<SearchField theme='white' value={searchValue} setValue={setSearchValue} />
-				</div>
+			</main>
 
-				<div className="flex-gap">
-					{visitorsList.filter((li) => {
-						return searchValue.toLowerCase() === '' ? li : li.name.toLowerCase().includes(searchValue);
-					}).map((li, id) => (
-						<VisitorCart key={id}
-							name={li.name}
-							subscriptionCounter={li.subscription}
-							minimized={true}
-							theme='light'
-						/>
-					))}
-				</div>
-			</PopupContainer>
-		</main>
+			<PopupAddVisitor
+				visitorsList={visitorsList}
+				isOpen={isPopupOpen}
+				closePopup={() => setIsPopupOpen(false)}
+				addVisitorFunc={addVisitor}
+			/>
+		</>
 	)
 }
