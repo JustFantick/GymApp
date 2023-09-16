ef45095d47a925823873517e60e7ef4b0920b578
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-09-16 11:20:21 +0300

    feat(visitors): add new props into SearchField
    Implemented input management props: value, setvalue. Added "theme" prop

--- src/components/search-field/search-field.jsx
+++ src/components/search-field/search-field.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import './search-field.less';
 import searchIcon from "../../img/search-ico.svg";
 
-export default function SearchField() {
+export default function SearchField(
+	{ theme = 'green', value = '', setValue }
+) {
 	return (
-		<label className='search-field'>
+		<label className={`search-field ${theme}`}>
 			<div className="search-field__icon">
 				<img src={searchIcon} alt="search-icon.svg" />
 			</div>
-			<input className="search-field__input" type="search" name='find-visitor' placeholder='Кого шукаємо?' />
+			<input
+				value={value}
+				onChange={(e) => setValue(e.target.value)}
+				className="search-field__input"
+				type="search" name='find-visitor'
+				placeholder='Кого шукаємо?'
+			/>
 		</label>
 	)
 }
