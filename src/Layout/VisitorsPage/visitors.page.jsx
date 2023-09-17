700a45682bb3fefa19763d216c7645081353cb0f
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-09-17 18:34:14 +0300

    feat(visitors): update VisitorPage
    Implemented opportunity to search visitor, added auto-animation to visitorsList

--- src/Layout/VisitorsPage/visitors.page.jsx
+++ src/Layout/VisitorsPage/visitors.page.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './visitors.page.less';
 import SearchField from '../../components/search-field/search-field.jsx';
 import VisitorCart from '../../components/visitor-cart/visitor-cart.jsx';
 import { useVisitorsStore } from '../../store/store';
 import PageContainer from '../../components/page-container/page-container.jsx';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 
 export default function VisitorsPage() {
 	const visitorsList = useVisitorsStore(state => state.visitors);
+	const [searchValue, setSearchValue] = useState('');
+	const [listRef] = useAutoAnimate();
 
 	return (
 		<div className='visitors'>
-			<SearchField />
+			<SearchField value={searchValue} setValue={setSearchValue} />
 
 			<PageContainer>
-				<div className="visitors__list">
+				<div className="visitors__list" ref={listRef}>
 					{
-						visitorsList.map((li, id) => (
-							<VisitorCart key={id}
+						visitorsList.filter((li) => {
+							return searchValue.toLowerCase() === '' ?
+								li :
+								li.name.toLowerCase().includes(searchValue.toLowerCase());
+						}).map((li) => (
+							<VisitorCart key={li.id}
 								name={li.name}
 								subscriptionCounter={li.subscription}
 								showSubscriptionCounter={false}
