da641da3f7e6ce43df96f251f9f4fae92cb10def
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-09-17 19:26:10 +0300

    feat(registration): improve InputName
    Added opportunity to control valid status and change label from props

--- src/components/form-inputs/input-name.jsx
+++ src/components/form-inputs/input-name.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import './form-inputs.less';
 import { InputWrapper } from "./input-wrapper.jsx";
 
-export const InputName = ({ name = '', setName }) => {
+export const InputName = ({ name = '', setName, label = 'ПІБ', isValid = true }) => {
 	return (
 		<InputWrapper>
-			<label className='input-wrapper__name-label'>
-				ПІБ
+			<label className={`input-wrapper__name-label ${isValid ? 'valid' : 'non-valid'}`}>
+				{label}
 				<input
 					type="text" name="name"
 					value={name} placeholder='______'
