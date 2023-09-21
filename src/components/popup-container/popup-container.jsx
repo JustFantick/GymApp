81466b2100773dd663e906e3a51aae707bdaf2b4
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-09-21 20:09:11 +0300

    feat: implement useScrollLock into all popups

--- src/components/popup-container/popup-container.jsx
+++ src/components/popup-container/popup-container.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './popup-container.less';
 import { motion } from 'framer-motion';
+import { useScrollLock } from '../../Layout/useScrollLock.jsx';
 
 export default function PopupContainer({ children, isOpen, closePopup }) {
+	const { lockScroll, unlockScroll } = useScrollLock();
+
+	useEffect(() => {
+		if (isOpen) {
+			lockScroll();
+		} else {
+			unlockScroll();
+		}
+	}, [isOpen]);
+
 	function bodyClickHandler(e) {
 		if (!e.target.closest('.popup__body')) {
 			closePopup();
 		}
 	}
+
 	return (
 		<motion.div className='popup'
 			onClick={bodyClickHandler}
