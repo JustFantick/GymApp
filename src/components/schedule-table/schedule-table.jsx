f7aeac4961215135d727a187db48023f42302cd0
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-10-06 20:21:33 +0300

    feat: improve ScheduleTable
    Implemented basic logic that renders row and column cells

--- src/components/schedule-table/schedule-table.jsx
+++ src/components/schedule-table/schedule-table.jsx
@@ -1,11 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './schedule-table.less';
 import DateIndicator from '../date-indicator/date-indicator.jsx';
 import moment from 'moment';
 import VisitorCartLink from '../visitor-cart/visitor-cart.jsx';
+import { useVisitorsStore } from '../../store/visitorStore';
 
 export default function ScheduleTable() {
 	const [date, setDate] = useState(moment());
+	const hoursArr = [
+		'12:00', '12:30',
+		'13:00', '13:30',
+		'14:00', '14:30',
+		'15:00', '15:30',
+		'16:00', '16:30',
+		'17:00', '17:30',
+		'18:00', '18:30',
+	];
+
+	const visitorsList = useVisitorsStore(state => state.visitors);
+	//const setTodaysVisitors = useVisitorsStore(state => state.setTodaysVisitors);
+	const [todaysVisitors, setTodaysVisitors] = useState([]);
+
+	useEffect(() => {
+		const temp = [];
+		visitorsList.forEach(visitorObj => {
+			if (visitorObj.schedule.find(d => date.day() == d.date).isActive) temp.push(visitorObj);
+		});
+		setTodaysVisitors(temp);
+	}, []);
 
 	return (
 		<table className="schedule-table">
@@ -20,41 +42,28 @@ export default function ScheduleTable() {
 			</thead>
 
 			<tbody>
-				<tr className='schedule-table__row'>
-					<th data-header-order='first'>12:00</th>
-					<th data-header-order='second'>
-						<VisitorCartLink
-							visitorId={0}
-							linkUrl={`/profile/${0}`}
-							name={'Денні Сінс lorem Денні Сінс lorem Денні Сінс'}
-							subscriptionCounter={1}
-							showDoneStatus={true}
-						/>
-						<VisitorCartLink visitorId={2} name={'Jonny Silverhand'} subscriptionCounter={0} />
-						<VisitorCartLink visitorId={3} name={'Olivia Stinks'} subscriptionCounter={1} />
-					</th>
+				{
+					hoursArr.map((hour, id) => (
+						<tr className='schedule-table__row' key={id}>
+							<th data-header-order='first'>{hour}</th>
+							<th data-header-order='second'>
+								{
+									todaysVisitors.filter(v => v.schedule[0].time == hour).map(v => (
+										<VisitorCartLink key={v.id}
+											visitorId={v.id}
+											name={v.name}
+											subscriptionCounter={v.subscription}
+											showDoneStatus={true}
+											reducePadding={true}
+										/>
+									))
+								}
+							</th>
 
-				</tr>
+						</tr>
+					))
+				}
 
-				<tr className='schedule-table__row'>
-					<th data-header-order='first'>12:00</th>
-					<th data-header-order='second'>
-						Danny Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, temporibus.<br />
-						Jonny <br />
-						Sam <br />
-						Bob <br />
-					</th>
-				</tr>
-
-				<tr className='schedule-table__row'>
-					<th data-header-order='first'>12:00</th>
-					<th data-header-order='second'>
-						Danny Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, temporibus.<br />
-						Jonny <br />
-						Sam <br />
-						Bob <br />
-					</th>
-				</tr>
 			</tbody>
 
 		</table>
