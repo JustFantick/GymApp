0cf0e3103874b100b33d2104409a581d370b5fcf
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-10-07 20:33:56 +0300

    feat: complete PopupDatePicker

--- src/components/popup-date-picker/popup-date-picker.jsx
+++ src/components/popup-date-picker/popup-date-picker.jsx
@@ -1,23 +1,40 @@
 import React from 'react';
 import './popup-date-picker.less';
 import { motion } from 'framer-motion';
+import { StaticDatePicker } from '@mui/x-date-pickers';
+import { CalendarLocalizator } from '../calendar/calendar-localizator.jsx';
 
-export default function PopupDatePicker({ isOpen, setDate, closePopup }) {
+export default function PopupDatePicker({ isOpen, closePopup, dateValue, setDate }) {
+	function outBodyClickHandler(e) {
+		if (!e.target.closest('.popup-date-picker__body')) closePopup();
+	}
+
+	function onDateChange(date) {
+		setDate(date);
+		setTimeout(closePopup, 450);
+	}
+
 	return (
 		<motion.div
 			className="popup-date-picker"
 			data-isopen={isOpen} layout
+			onClick={outBodyClickHandler}
 		>
 			<motion.div
 				className="popup-date-picker__body"
-				onClick={closePopup}
 				variants={{
-					closed: { scale: 0 },
-					open: { scale: 1 }
+					closed: { scale: 0, opacity: 0, transition: { type: 'spring', duration: 1 } },
+					open: { scale: 1, opacity: 1, transition: { type: 'spring', duration: 0.5 } }
 				}}
-				transition={{ type: 'spring', duration: 0.5 }}
 				animate={isOpen ? "open" : "closed"}
 			>
+				<CalendarLocalizator>
+					<StaticDatePicker
+						value={dateValue}
+						onChange={newDate => onDateChange(newDate)}
+						views={['day']}
+					/>
+				</CalendarLocalizator>
 
 			</motion.div>
 
