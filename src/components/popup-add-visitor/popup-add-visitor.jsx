b536e1f6445b4a9589aa2fc22c0393844fd6eb58
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-10-09 09:49:37 +0300

    feat: complete PopupAddVisitor

--- src/components/popup-add-visitor/popup-add-visitor.jsx
+++ src/components/popup-add-visitor/popup-add-visitor.jsx
@@ -1,48 +1,133 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import './popup-add-visitor.less';
 import PopupContainer from '../popup-container/popup-container.jsx';
 import SearchField from '../search-field/search-field.jsx';
-import VisitorCart from '../visitor-cart/visitor-cart.jsx';
+import { VisitorCart } from '../visitor-cart/visitor-cart.jsx';
 import { useAutoAnimate } from '@formkit/auto-animate/react';
+import ClockComponent from '../clock-component/clock-component.jsx';
 
-export default function PopupAddVisitor({ addVisitorFunc, visitorsList, isOpen, closePopup }) {
+import { AnimatePresence, motion } from 'framer-motion';
+import { useVisitorsStore } from '../../store/visitorStore.js';
+
+export default function PopupAddVisitor({
+	isOpen, closePopup, addVisitorFunc,
+}) {
 	const [searchValue, setSearchValue] = useState('');
 	const [listRef] = useAutoAnimate();
 
+	const visitorsList = useVisitorsStore(state => state.visitors);
+	const todaysVisitors = useVisitorsStore(state => state.todaysVisitors);
+
+	const [visitorsToRender, setVisitorsToRender] = useState([]);
+
+	useEffect(() => {
+		setVisitorsToRender(visitorsList.filter(li => todaysVisitors.map(el => el.id).indexOf(li.id) == -1));
+	}, [todaysVisitors]);
+
+	const [hour, setHour] = useState('12:00');
+
+	function setTime(newTimeValue) {
+		setHour(newTimeValue);
+		setTimeout(() => {
+			setSlideIndex(slideIndex + 1);
+		}, 500);
+	}
+
+	function addVisitorHandler(li) {
+		addVisitorFunc({ ...li, time: hour });
+		closePopupHandler();
+	}
+
+	function closePopupHandler() {
+		closePopup();
+		setTimeout(() => {
+			setSearchValue('');
+			setHour('12:00');
+			setSlideIndex(0);
+		}, 500);
+	}
+
+	const [clockComponentRef, chooseVisitorRef] = [useRef(null), useRef(null)];
+
+	const [slideIndex, setSlideIndex] = useState(0);
+	const slides = [
+		{
+			refObj: clockComponentRef,
+			block:
+				<div ref={clockComponentRef} className='clock-slide'>
+					<div className="clock-slide__header">
+						<h4>Оберіть час</h4>
+						<span onClick={() => setSlideIndex(slideIndex + 1)}></span>
+					</div>
+
+					<ClockComponent
+						hour={hour}
+						setTime={setTime}
+					/>
+
+				</div>,
+		},
+		{
+			refObj: chooseVisitorRef,
+			block:
+				<div className="add-visitor-slide" ref={chooseVisitorRef}>
+					<div
+						className="add-visitor-slide__visitors-list"
+						ref={listRef}
+					>
+						{
+							visitorsToRender.filter((li) => {
+								return searchValue.toLowerCase() === '' ?
+									li :
+									li.name.toLowerCase().includes(searchValue.toLowerCase());
+							}).map(li => (
+								<VisitorCart key={li.id}
+									name={li.name}
+									subscriptionCounter={li.subscription}
+									theme='light'
+									onClick={() => addVisitorHandler(li)}
+								/>
+							))
+						}
+
+					</div>
+
+					<div className="add-visitor-slide__search-field">
+						<SearchField theme='white' value={searchValue} setValue={setSearchValue} />
+					</div>
+
+				</div>
+		}
+	]
+
 	return (
-		<PopupContainer isOpen={isOpen} closePopup={closePopup}>
-			<div className="add-visitor-popup"
-				style={{
-					display: 'flex',
-					flexDirection: 'column',
-					gap: '20px',
+		<PopupContainer isOpen={isOpen} closePopup={closePopupHandler}>
+			<motion.div
+				style={{ position: 'relative' }}
+				animate={{
+					height: slides[slideIndex].refObj.current && slides[slideIndex].refObj.current.clientHeight,
 				}}
 			>
-				<div className="add-visitor-popup__search-field">
-					<SearchField theme='white' value={searchValue} setValue={setSearchValue} />
-				</div>
+				<AnimatePresence>
+					<motion.div
+						key={slideIndex}
+						style={{
+							position: 'absolute',
+							bottom: 0,
+							width: '100%',
+							height: '100%',
+						}}
+						initial={{ x: '110%' }}
+						animate={{ x: 0 }}
+						exit={{ x: '-110%' }}
+						transition={{ duration: 0.5, type: 'spring' }}
+					>
+						{slides[slideIndex].block}
+					</motion.div>
 
-				<div
-					className="add-visitor-popup__visitors-list flex-gap"
-					ref={listRef}
-					style={{ overflowY: 'scroll' }}
-				>
-					{visitorsList.filter((li) => {
-						return searchValue.toLowerCase() === '' ?
-							li :
-							li.name.toLowerCase().includes(searchValue.toLowerCase());
-					}).map((li, id) => (
-						<VisitorCart key={id}
-							name={li.name}
-							subscriptionCounter={li.subscription}
-							showSubscriptionCounter={false}
-							theme='light'
-							preventNavLink={true}
-							onClick={() => addVisitorFunc(li)}
-						/>
-					))}
-				</div>
+				</AnimatePresence>
 
-			</div>
+			</motion.div>
 
 		</PopupContainer >
 	)
