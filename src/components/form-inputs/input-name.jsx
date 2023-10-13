144519ebe661f66daa36e1d7c60fef937ec6cdca
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-10-13 20:59:33 +0300

    feat: unfocus InputName after Enter was pressed

--- src/components/form-inputs/input-name.jsx
+++ src/components/form-inputs/input-name.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { InputWrapper } from "./input-wrapper.jsx";
 
 export const InputName = ({ name = '', setName, label = '', isValid = true }) => {
+	function onKeyDown(e) {
+		if (e.key === 'Enter') e.target.blur();
+	}
+
 	return (
 		<InputWrapper isValid={isValid}>
 			<label className="input-wrapper__name-label">
@@ -10,6 +14,7 @@ export const InputName = ({ name = '', setName, label = '', isValid = true }) =>
 					type="text" name="name"
 					value={name} placeholder='______'
 					onChange={(e) => setName(e.target.value)}
+					onKeyDown={onKeyDown}
 				/>
 			</label>
 		</InputWrapper>
