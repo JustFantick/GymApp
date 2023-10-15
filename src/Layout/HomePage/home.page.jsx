e67e019dd7c39604cf001fc1ff822886276d51fc
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-10-15 19:25:49 +0300

    feat(home): implement todayDate check
    OnMount check on current date, when dates are different set actual value of "todayDate" and set "todayVisitors" an empty array

--- src/Layout/HomePage/home.page.jsx
+++ src/Layout/HomePage/home.page.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './home.page.less';
 import ScheduleTable from '../../components/schedule-table/schedule-table.jsx';
+import { useVisitorsStore } from '../../store/visitorStore';
+import moment from 'moment';
 
 export default function HomePage() {
+	const todayDate = useVisitorsStore(state => state.todayDate);
+	const setTodayDate = useVisitorsStore(state => state.setTodayDate);
+	const setTodaysVisitors = useVisitorsStore(state => state.setTodaysVisitors);
+
+	useEffect(() => {
+		if (todayDate === null || todayDate !== moment().dayOfYear()) {
+			setTodayDate(moment().dayOfYear());
+			setTodaysVisitors([]);
+		}
+	}, []);
+
 	return (
 		<main className='home'>
 			<div className="home__body">
